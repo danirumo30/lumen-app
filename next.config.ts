51d3ccfd642662da4e78bff56f0470c05eb0e82f
danirumo30 <danirumo30@gmail.com>
2026-03-24 13:01:21 +0100

    fix(next.config): add image domains for TMDB, IGDB, GitHub avatars

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,31 @@ const nextConfig: NextConfig = {
     serverActions: {
       bodySizeLimit: '2mb',
     },
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.igdb.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.githubusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
